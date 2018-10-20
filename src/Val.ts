7bc2f23bef3d67a73818cdfd3e1c49ffde14af42
Srđan Milaković <srdjan@rice.edu>
2018-10-20 18:56:37 -0500

    Added Val#asShortHexString.

--- src/Val.ts
+++ src/Val.ts
@@ -1,51 +1,52 @@
-
-
-
-
 export default class Val {
-    readonly val : number;
-    readonly num_bits : number;
+    readonly val: number;
+    readonly num_bits: number;
 
-    private static mod(n : number, m : number) :number {
+    private static mod(n: number, m: number): number {
         return ((n % m) + m) % m;
     }
 
-    constructor(val : number = 0, num_bits : number) {
+    constructor(val: number = 0, num_bits: number) {
         this.num_bits = num_bits;
         this.val = val;
     }
 
-    static UnsignedInt(val : number, num_bits : number = 32) : Val {
+    static UnsignedInt(val: number, num_bits: number = 32): Val {
         return new Val(Val.mod(val, 2 ** num_bits), num_bits);
     }
 
-    asUnsignedInt() : number {
+    asUnsignedInt(): number {
         return this.val;
     }
 
-    static SignedInt(val : number, num_bits : number = 32) : Val {
+    static SignedInt(val: number, num_bits: number = 32): Val {
         const max_signed = 2 ** (num_bits - 1) - 1;
-        if(val >= 0)
+        if (val >= 0)
             return new Val(Val.mod(val, max_signed), num_bits);
-        if(val < 0)
+        if (val < 0)
             return new Val((max_signed + 1) - Val.mod(val, max_signed), num_bits);
     }
 
-    asSignedInt() : number {
+    asSignedInt(): number {
         const max_signed = 2 ** (this.num_bits - 1) - 1;
-        if(this.val <= max_signed)
+        if (this.val <= max_signed)
             return this.val;
         return -(this.val - (max_signed + 1));
     }
 
-    asHexString() : string {
+    asHexString(): string {
         let str = this.asUnsignedInt().toString(16);
-        while(str.length < this.num_bits/4)
+        while (str.length < this.num_bits / 4) {
             str = "0" + str;
+        }
         return "0x" + str.toUpperCase();
     }
 
-    getNumBits() : number {
+    asShortHexString(): string {
+        return this.asUnsignedInt().toString(16).toLocaleUpperCase();
+    }
+
+    getNumBits(): number {
         return this.num_bits;
     }
 
@@ -56,9 +57,9 @@ export default class Val {
         console.log(this.UnsignedInt(-123).asSignedInt());
         console.log(this.UnsignedInt(123231).asSignedInt());
         console.log(this.UnsignedInt(123412).asSignedInt());
-        console.log(this.UnsignedInt(2**32).asSignedInt());
-        console.log(this.UnsignedInt(2**32-1).asSignedInt());
-        console.log(this.UnsignedInt(2**32+1).asSignedInt());
+        console.log(this.UnsignedInt(2 ** 32).asSignedInt());
+        console.log(this.UnsignedInt(2 ** 32 - 1).asSignedInt());
+        console.log(this.UnsignedInt(2 ** 32 + 1).asSignedInt());
 
         console.log(this.SignedInt(1).asSignedInt());
         console.log(this.SignedInt(0).asSignedInt());
@@ -67,7 +68,6 @@ export default class Val {
         console.log(this.SignedInt(-1234567).asSignedInt());
         console.log(this.SignedInt(7654321).asSignedInt());
         console.log(this.SignedInt(999999999999999).asHexString());
-
 
 
     }
